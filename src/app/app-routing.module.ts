fd60341bd3f977d5b315745d7374cc6465e68045
Nikita Raizada <nikraiza@publicisgroupe.net>
2021-05-22 17:23:45 +0530

    Publicis Sapient Assignement

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./floating-banner/floating-banner.module').then(m => m.FloatingBannerModule)
+  },
+  {
+    path: 'e-commerce',
+    loadChildren: () => import('./e-commerce/e-commerce.module').then(m => m.ECommerceModule)
+  },
+  {
+    path: 'dynamic-table',
+    loadChildren: () => import('./dynamic-table/dynamic-table.module').then(m => m.DynamicTableModule)
+  },
+  {
+    path: 'io-countdown',
+    loadChildren: () => import('./input-output-countdown-timer/input-output-countdown-timer.module').then(m => m.InputOutputCountdownTimerModule)
+  },
+  {
+    path: 'subject-countdown',
+    loadChildren: () => import('./subject-countdown-timer/subject-countdown.module').then(m => m.SubjectCountDownModule)
+  },
+  {
+    path: 'dynamic-divs', 
+    loadChildren: () => import('./dynamic-divs/dynamic-divs.module').then(m => m.DynamicDivsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { SubjectCountdownService } from './subject-countdown-timer/subject-countdown.service';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule, 
+    SharedModule,
+    CoreModule
+  ],
+  providers: [SubjectCountdownService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
