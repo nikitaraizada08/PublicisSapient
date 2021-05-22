258f3b3460fe8851f8de13ba3b85b1f768349e56
Nikita Raizada <nikraiza@publicisgroupe.net>
2021-05-22 18:43:22 +0530

    Publicis Sapient Assignement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/floatingbanner', pathMatch: 'full' },
   {
-    path: '',
+    path: 'floatingbanner',
     loadChildren: () => import('./floating-banner/floating-banner.module').then(m => m.FloatingBannerModule)
   },
   {
@@ -29,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
